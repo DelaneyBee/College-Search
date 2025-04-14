1c7c84e4bd2ddf4f07475b33f8a8981585ea5375
DelaneyBee <dablasz6@gmail.com>
2025-04-14 09:29:25 -0500

    Initial Commit

---
+++ College Search/College_SearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  College_SearchApp.swift
+//  College Search
+//
+//  Created by Delaney Blaszinski on 4/14/25.
+//
+
+import SwiftUI
+
+@main
+struct College_SearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
