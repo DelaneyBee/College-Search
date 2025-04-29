8f1703838fe68002468daff8f4afd8def55b9937
Samantha Hiles <samih11@att.net>
2025-04-29 09:35:27 -0500

    Updated initial view options

--- College Search/InitialView.swift
+++ College Search/InitialView.swift
@@ -17,7 +17,7 @@ struct ButtonDropdownView: View {
    
 
     let size = ["0-1000", "1001-5000", "5001-10000", "10001-25000", "25001-50000", "50001+"]
-    let distance = ["0-25 miles", "26-50 miles", "51-100 miles", "101-200 miles", "201-500 miles"]
+    let distance = ["0-25 miles", "26-50 miles", "51-100 miles", "101-200 miles", "201-500 miles", "501+ miles"]
    
 
     var body: some View {
