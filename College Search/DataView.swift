1312a7831c40025e81db1891c4c060e18f183d1e
DelaneyBee <dablasz6@gmail.com>
2025-05-01 09:52:42 -0500

    Button and Data view start

---
+++ College Search/DataView.swift
@@ -0,0 +1,16 @@
+//
+//  DataView.swift
+//  College Search
+//
+//  Created by Delaney Blaszinski on 5/1/25.
+//
+
+import SwiftUI
+
+struct DataView: View {
+    var body: some View {
+        VStack {
+            
+        }
+    }
+}
--- College Search/InitialView.swift
+++ College Search/InitialView.swift
@@ -84,8 +84,14 @@ struct ButtonDropdownView: View {
                         }
                     }
                 }
-                // Animal Dropdown
+              
                 
+                NavigationLink {
+                    //View where API would be
+                      //
+                } label: {
+                    CustomButton(text: "Load")
+                }
                 
                 Spacer()
                     .navigationTitle("College Search")
@@ -94,7 +100,7 @@ struct ButtonDropdownView: View {
             .font(Font.custom("Times New Roman", size: 18))
             .background(Color.pink).opacity((0.6))
         }
-        
+       
         
     }
    
