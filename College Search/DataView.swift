da96133426611522d5ee156c3b5c10162a98c74e
DelaneyBee <dablasz6@gmail.com>
2025-05-01 09:59:18 -0500

    Got Data View and Game Manger working together

--- College Search/DataView.swift
+++ College Search/DataView.swift
@@ -4,10 +4,11 @@
 //
 //  Created by Delaney Blaszinski on 5/1/25.
 //
-
+import Foundation
 import SwiftUI
 
 struct DataView: View {
+    @EnvironmentObject var gameManger: GameManger
     var body: some View {
         VStack {
             
