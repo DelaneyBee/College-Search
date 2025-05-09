a6e388fe096bad496e8fd7e5afe6f24a7a18ca41
DelaneyBee <dablasz6@gmail.com>
2025-05-09 09:52:06 -0500

    ????

--- College Search/CollegeSearchView.swift
+++ College Search/CollegeSearchView.swift
@@ -79,7 +79,7 @@ struct CollegeSearchView: View {
                 }
                 .padding(.horizontal)
                 Spacer()
-                Image("Grad Cap").resizable().frame(width: 170, height: 170)
+                Image("Grad Cap").resizable().frame(width: 150, height: 150)
                    
             }
             .padding()
